40f36fe74a08ba38fb4a342d74e22707e6e7334d
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2022-02-23 13:51:41 +0000

    Listing models subpackage in main __init__.py file.

--- rigelcore/__init__.py
+++ rigelcore/__init__.py
@@ -1,5 +1,6 @@
 from . import clients  # noqa: F401
 from . import loggers  # noqa: F401
+from . import models  # noqa: F401
 from .exceptions import (  # noqa: F401
     RigelError,
     DockerImageNotFoundError,
